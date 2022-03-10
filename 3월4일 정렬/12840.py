22dec0b409c778bcf2b2fb6c07507969af296b28
jhyii <83736896+jhyii@users.noreply.github.com>
2022-03-10 23:56:46 +0900

    p1 수정했습니다

--- 3월4일 정렬/12840.py
+++ 3월4일 정렬/12840.py
@@ -1,26 +1,23 @@
+import datetime
+
 h, m, s = map(int, input().split()) #현재시간 입력받음
 n = int(input())
 
 TOTAL_SEC = h*3600 + m*60 + s #현재시간을 초로 계산해놓기
-UPPER_SEC = 24*3600 
 
 def time_cul(): #전체 초로 시간,분,초를 계산하는 함수
-    global TOTAL_SEC, UPPER_SEC
-    while TOTAL_SEC < 0 or TOTAL_SEC > UPPER_SEC : #전체 초가 마이너스이거나 24시간을 넘는 경우
-        if TOTAL_SEC < 0 : #전체 초가 마이너스인 경우 24시간에 해당하는 초를 더해준다
-            TOTAL_SEC += UPPER_SEC
-        elif TOTAL_SEC > UPPER_SEC : #전체 초가 24시간보다 넘는 경우 24시간에 해당하는 라운드만큼 초를 없애준다
-            TOTAL_SEC %= UPPER_SEC
-        
-    hour = int(TOTAL_SEC/3600) #전체 초를 3600초로 나눈 몫
-    minute = int((TOTAL_SEC%3600)/60) #전체 초를 3600초로 나눈 나머지를 다시 60초로 나눈 몫
-    second = int(TOTAL_SEC%60) #전체 초를 60초로 나눈 나머지
-    
+    global TOTAL_SEC
+    time_str = (lambda x : '0' + x if len(x) < 8 else x )(str(datetime.timedelta(seconds=TOTAL_SEC)))
+
+    hour = int(time_str[:2])
+    minute = int(time_str[3:5])
+    second = int(time_str[6:8])
+
     print(hour, minute, second)
-    
+
 for i in range(n):
     query = input()
-    
+
     if query[0] == '1':
         TOTAL_SEC += int(query[2:]) #전체 초에 더해준다
     elif query[0] == '2':
--- 3월4일 정렬/1946.py
+++ 3월4일 정렬/1946.py
@@ -1,27 +1,21 @@
 case = int(input())
 
-def decide(grade, n):
-    count = n #지원자 수
-    lose = set() #탈락하는 지원자
-    
-    for z in range(n):
-        temp_grade = grade.copy() 
-        del temp_grade[z] #1명의 지원자와 이 지원자와 비교할 남은 지원자들의 명단 
-        
-        for a in temp_grade.keys():
-            if grade[z][0] > temp_grade[a][0] and grade[z][1] > temp_grade[a][1] : #이 지원자의 순위가 둘다 낮으면 탈락하는 지원자가 됨
-                lose.add(z)
-                
-    count -= len(lose) #탈락자 수를 뺌
-    print(count)
-
 for i in range(case):
-    
+
     n = int(input())
-    grade = {} 
-    
+    grade = []
+
     for j in range(n):
-        a, b = map(int, input().split()) #입력받은 점수를 딕셔너리로 저장
-        grade[j] = [a,b]
-        
-    decide(grade, n)
+        a, b = map(int, input().split()) #입력받은 점수를 이중리스트로 저장
+        grade.append([a,b])
+
+    grade.sort() #첫번째등수로 정렬
+    max = grade[0][1] #첫번째등수 1등인 사람의 두번째등수 기준
+    count = 1 #1등은 무조건 합격이라 카운트
+
+    for i in range(1,len(grade)):
+        if grade[i][1] < max : #두번째등수가 max보다 작으면 합격
+            count += 1
+            max = grade[i][1] #다음 사람은 첫번째 등수 바로 앞의 사람과 두번째등수를 비교해야함
+
+    print(count)
